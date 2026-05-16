92601a0547bf85798b5136d6234bd5785a0e2648
Diabl0269 <talefronny@gmail.com>
2026-05-16 20:23:38 +0300

    fix(UI): add virtual destructor and leak detector macro to ArrangementView

--- Source/UI/ArrangementView.h
+++ Source/UI/ArrangementView.h
@@ -4,6 +4,10 @@
 class ArrangementView : public juce::Component {
 public:
     ArrangementView();
+    ~ArrangementView() override = default;
     void paint(juce::Graphics& g) override;
     void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ArrangementView)
 };
